58be42fa9e47bbca8612d6bdb3c243e43626510b
Jaywooski <john.m.wooley@gmail.com>
2022-05-21 18:41:29 -0500

    Getting script.js together still!

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,13 @@
 const responseArea = document.querySelector('#prompt-area')
 const submitBtn = document.querySelector('#text-button');
+const formInput = document.querySelector('.form-input');
 
 function buttonHandler(e) {
     e.preventDefault();
-
-   
+    var content = formInput.value;
+    console.log(content);
+    formInput.value = '';
+    
 }
 
 submitBtn.addEventListener('click', buttonHandler);
