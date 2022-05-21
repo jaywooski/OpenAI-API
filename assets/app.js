8042990533f4539bcacfb9778bac02c3b2d45b0d
Jaywooski <john.m.wooley@gmail.com>
2022-05-21 18:24:34 -0500

    Working on script

---
+++ assets/app.js
@@ -0,0 +1,10 @@
+const responseArea = document.querySelector('#prompt-area')
+const submitBtn = document.querySelector('#text-button');
+
+function buttonHandler(e) {
+    e.preventDefault();
+
+   
+}
+
+submitBtn.addEventListener('click', buttonHandler);
